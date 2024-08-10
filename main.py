eb8750efab4c96e7c710f2dadc7ad8ed15ec81a2
DacKhoa7-6 <anhkhoadac3101@gmail.com>
2024-08-10 15:47:33 -0400

    part 3 - complete

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ plt.style.use('fivethirtyeight')
 
 #Get the stock quote 
 # df = web.DataReader('AAPL', data_source='yahoo', start='2012-01-01', end='2019-12-31')
-df = yf.download('AAPL', start='2012-01-01', end='2019-12-31')
+df = yf.download('AAPL', start='2012-01-01', end='2019-12-19')
 #Show the data 
 print(df)
 
 #Visualize the closing price 
-# plt.figure(figsize=(16,8))
-# plt.title('Close Price History')
-# plt.plot(df['Close'])
-# plt.xlabel('Date', fontsize=18)
-# plt.ylabel('Close Price USD ($)', fontsize=18)
-# plt.show()
+plt.figure(figsize=(16,8))
+plt.title('Close Price History')
+plt.plot(df['Close'])
+plt.xlabel('Date', fontsize=18)
+plt.ylabel('Close Price USD ($)', fontsize=18)
+plt.show()
 
 #Create a new dataframe with only the "close column"
 data = df.filter(['Close'])
@@ -86,7 +86,8 @@ x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
 #Get the model's predeicted price values 
 predictions = model.predict(x_test)
-predictions = scaler.inverse_transform(predictions) # here we want predictions to contain the same values as y_test values
+predictions = scaler.inverse_transform(predictions) # since predictions currently hold the scaled predicted price, we have to inverse transform it to the actual price.
+# here we want predictions to contain the same values as y_test values
 
 #Get the root mean squared error (RMSE) - the lower value indicates a better fit, a value of 5 is pretty decent. 
 rmse = np.sqrt(np.mean(predictions - y_test)**2)
@@ -95,7 +96,7 @@ print(rmse)
 #Plot the data 
 train = data[0:training_data_len]
 validation = data[training_data_len:]
-valid['Predictions'] = predictions
+validation['Predictions'] = predictions
 #Visualize the data 
 plt.figure(figsize=(16,8))
 plt.title('Model')
@@ -107,13 +108,39 @@ plt.legend(['Train', 'Validation', 'Predictions'], loc = 'lower right')
 plt.show()
 
 
+#Show the valid and predicted prices 
+print(validation)
+###################################################################################################################################
+#Below is to get a single predicted price
+#Get the quote 
+apple_quote = yf.download('AAPL', start='2012-01-01', end='2019-12-17')
 
+#Create a new dataframe 
+new_df = apple_quote.filter(['Close'])
 
+#Get the last 60 days values and convert the dataframe to an np array 
+last_60_days = new_df[-60:].values
 
+#Scale the data to be values between 0 and 1
+last_60_days_scaled = scaler.transform(last_60_days) # we are not using the fit transform because we want to transform the data using the min and max values 
 
+#Creatre an empty test list and append the last 60 days data to the test list 
+X_test = []
+X_test.append(last_60_days_scaled)
 
+#Convert the dataset to an np array 
+X_test = np.array(X_test)
 
+#Reshape the data 
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
+#Get the predicted scaled price for 12/18 
+pred_price = model.predict(X_test)
 
+#Undo the scaling 
+pred_price = scaler.inverse_transform(pred_price)
+print(pred_price)
 
-
+#Get the actual price 
+apple_quote_2 = yf.download('AAPL', start='2019-12-18', end='2019-12-19')
+print(apple_quote_2['Close'])
